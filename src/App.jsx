1fdc6a9fc88cfda4716779cf69f4c86b6ed7ecd2
Dhanraj Jadhav <dj004786@gmail.com>
2024-12-24 12:16:16 +0530

    first commit Login and singup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import farmImage from ;
+import Login from './components/Login'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Login/>
+
+    </>
+  )
+}
+
+export default App
