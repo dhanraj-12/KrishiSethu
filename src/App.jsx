0d88e6e838a36b54414abc801617120d8662f350
Vishwadeep112004 <vishwadeep.sutar@walchandsangli.ac.in>
2025-04-02 09:28:12 +0530

    okay

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,28 @@
-// import farmImage from ;
-import Login from './components/Login'
-import RequestComp from './components/RequestComp'
-import ProgressBar from './components/ProgressBar'
-import Content_on_home_page from './components/Content_on_home_page'
-import { createBrowserRouter, RouterProvider} from 'react-router-dom';
-import Content_on_selling_page from './components/Content_on_selling_page';
-import CommingSoon from './components/commingsoon';
-import NavBar from './components/NavBar';
-import Profile from './components/Profile';
-import './App.css'
-import BulkOption from './components/BulkOption';
-import Articles from './components/Articles';
-
-const router = createBrowserRouter([
-  {
-    path : '/',
-    element : <Content_on_home_page/>
-  },
-
-  {
-    path : '/Auth',
-    element : <Login/>
-  },
-
-  {
-    path : '/Retail',
-    element : <CommingSoon/>
-  },
-  {
-    path :  '/Bulk',
-    element : <BulkOption/>
-  },
-  {
-    path : '/Profile',
-    element : <Profile />
-  },
-  {
-    path : '/Articles',
-    element : <Articles/>
-  },
-  {
-    path : '/Bulkbuy',
-    element : <RequestComp/>
-  },
-  {
-    path : '/Bulksell',
-    element : <Content_on_selling_page/>
-  } 
-])
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Content_on_home_page from "./components/Content_on_home_page";
+import Login from "./components/Login";
+import CommingSoon from "./components/commingsoon";
+import BulkOption from "./components/BulkOption";
+import Profile from "./components/Profile";
+import Articles from "./components/Articles";
+import RequestComp from "./components/RequestComp";
+import Content_on_selling_page from "./components/Content_on_selling_page";
 
 function App() {
   return (
-    <>
-     <RouterProvider router = {router}/>
-     {/*  */}
-    
-      {/* <Profile></Profile> */}
-
-      {/* <Content_on_selling_page /> */}
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Content_on_home_page />} />
+        <Route path="/Auth" element={<Login />} />
+        <Route path="/Retail" element={<CommingSoon />} />
+        <Route path="/Bulk" element={<BulkOption />} />
+        <Route path="/Profile" element={<Profile />} />
+        <Route path="/Articles" element={<Articles />} />
+        <Route path="/Bulkbuy" element={<RequestComp />} />
+        <Route path="/Bulksell" element={<Content_on_selling_page />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
